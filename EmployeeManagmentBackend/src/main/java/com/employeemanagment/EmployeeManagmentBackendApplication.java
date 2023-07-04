e602b4375fbfff4df36fe83c6822c263e0501eb3
Anton Usiankou <74109426+AntonUsiankou@users.noreply.github.com>
2023-07-04 16:14:00 +0300

    Add files via upload

---
+++ EmployeeManagmentBackend/src/main/java/com/employeemanagment/EmployeeManagmentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.employeemanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagmentBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagmentBackendApplication.class, args);
+    }
+
+}
